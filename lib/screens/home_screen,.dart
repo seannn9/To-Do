adebf0d6d7db1508dd97e5632c7f6661e253ce3a
seannn9 <naeskirlu9@gmail.com>
2024-08-22 21:17:33 +0800

    added non functional buttons in bottom app bar

--- lib/screens/home_screen,.dart
+++ lib/screens/home_screen,.dart
@@ -12,8 +12,22 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      body: const Stack(
+      body: Stack(
         children: [
+          Container(
+            margin: const EdgeInsets.all(50.0),
+            height: 100.0,
+            // color: Colors.white,
+            child: const Align(
+              alignment: Alignment.topCenter,
+              child: Text(
+                'No Tasks Added',
+                style: TextStyle(
+                  fontSize: 30.0,
+                ),
+              ),
+            ),
+          )
         ],
       ),
       floatingActionButton: FloatingActionButton(
@@ -24,8 +38,24 @@ class _HomePageState extends State<HomePage> {
         },
         child: const Icon(Icons.add),
       ),
-      bottomNavigationBar: const BottomAppBar(
-        color: Colors.deepPurpleAccent
+      bottomNavigationBar: BottomAppBar(
+        height: 60.0,
+        color: Colors.deepPurpleAccent,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.home, color: Colors.white),
+            ),
+            IconButton(onPressed: () {},
+                icon: const Icon(Icons.search, color: Colors.white)
+            ),
+            IconButton(onPressed: () {},
+                icon: const Icon(Icons.calendar_month, color: Colors.white)
+            )
+          ],
+        )
       ),
     );
   }
